e18558269646ed16ee201aebc0937b23c6c6a807
TimothyYeou <timyeou1234@hotmail.com>
2016-08-04 14:50:43 +0800

    Initial Commit

---
+++ Assessment_1-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assessment_1-2
+//
+//  Created by YeouTimothy on 2016/8/4.
+//  Copyright © 2016年 YeouTimothy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
